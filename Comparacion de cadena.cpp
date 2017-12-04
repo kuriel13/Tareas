2c783e9da7554eefa29cf650a15aaf5e714e11d1
kuriel13 <31073424+kuriel13@users.noreply.github.com>
2017-12-04 00:11:46 -0600

    Update Comparacion de cadena.cpp

--- Comparacion de cadena.cpp
+++ Comparacion de cadena.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
-#include <string>
-using namespace std;
+#include <cstring>
 
-int main()
+using namespace std;
+int buscarC(char fras[], char pal[]);
+
+int main(int argc, char** argv) {
+	char frase[50], palabra[10];
+	int posi;
+	
+	cout<<"Ingresa una frase: ";
+	cin.getline(frase,50);
+	cout<<"Ingresa la palabra a buscar: ";
+	cin.getline(palabra,10);
+	posi=buscarC(frase,palabra);
+	if (posi==1)
+        cout<<"No se encuentra la palabra";
+    else
+        cout<<"La palabra esta en la posicion: "<<posi;
+	return 0;
+}
+int buscarC(char fras[], char pal[])
 {
-    char texto[]={"Ora pues pinches perros"}, palabra[20];
-    int i=0, k;
-    bool x=1;
-    
-    cin.getline(palabra,20);
-    
-    do{
-        if(texto[i]==palabra[0])
-            for(int j=1;palabra[j]!=0;j++)
-                if(texto[i+j]!=palabra[j])
-                    x=0;
-        i++;
-    }while(x==0);
-    cout<<i;
-            
-    return 0;
+    int conta=0, conta2=0;
+    for(;conta<=strlen(fras);)
+    {
+        if(pal[conta2]==fras[conta])
+        {
+            conta++;
+			conta2++;
+            if(conta2==strlen(pal))
+                return (conta-conta2);
+        }
+        else
+        {
+            if (conta2==0)
+                conta++;
+            else
+                conta+=conta2-1, conta2=0;
+        }
+    }
+    return 1;
 }
